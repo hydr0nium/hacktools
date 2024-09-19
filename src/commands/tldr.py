7376cededcdede524538894efabfb76f2e51a658
hydr0nium <37932436+hydr0nium@users.noreply.github.com>
2024-09-19 19:57:50 +0200

    Implemented tldr feature

--- src/commands/tldr.py
+++ src/commands/tldr.py
@@ -1,2 +1,11 @@
-def main(args, config):
-	raise NotImplemented
+import requests
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from argparse import Namespace
+
+def main(args: 'Namespace', config):
+	r = requests.get("https://cheat.sh/" + args.executable)
+	print(r.content.decode())
+	print("\n\033[31;1mDisclaimer: tldr function is a wrapper for cheat.sh. For more see https://github.com/chubin/cheat.sh")
+	print("\033[31;1mTo get a better experience pipe the output to the 'more' or 'less' command")
